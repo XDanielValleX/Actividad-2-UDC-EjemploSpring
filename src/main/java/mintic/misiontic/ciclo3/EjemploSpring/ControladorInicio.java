77a757330c9662c8c1f75f8f23d652c6d3e0d817
Daniel Valle <danielvalleortiz@gmail.com>
2026-06-02 23:23:55 -0500

    feat(guides-6): intercambio de objetos (Usuario)

--- src/main/java/mintic/misiontic/ciclo3/EjemploSpring/ControladorInicio.java
+++ src/main/java/mintic/misiontic/ciclo3/EjemploSpring/ControladorInicio.java
@@ -6,6 +6,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import lombok.extern.slf4j.Slf4j;
+import mintic.misiontic.ciclo3.modelo.Usuario;
 
 @Controller
 @Slf4j
@@ -19,6 +20,14 @@ public class ControladorInicio {
         String mensaje = "Saludos desde Spring MVC con paso de información";
         modelo.addAttribute("mensaje", mensaje);
         modelo.addAttribute("dato", dato);
+
+        Usuario u = new Usuario();
+        u.setCedula("1234");
+        u.setClave("Abcd");
+        u.setNombre("JOHN CARLOS ARRIETA ARRIETA");
+        u.setEmail("jarrieta@hotmail.com");
+        modelo.addAttribute("alguien", u);
+
         log.info("Ejecutando el controlador Inicio MVC");
         return "index";
     }
