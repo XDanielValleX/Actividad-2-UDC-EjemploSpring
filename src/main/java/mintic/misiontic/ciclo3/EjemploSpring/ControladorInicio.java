d0ccce730f99aaad33ee0297829288ce73f9b46c
Daniel Valle <danielvalleortiz@gmail.com>
2026-06-02 23:28:26 -0500

    feat(guides-7): pasar lista de Usuario a la vista

--- src/main/java/mintic/misiontic/ciclo3/EjemploSpring/ControladorInicio.java
+++ src/main/java/mintic/misiontic/ciclo3/EjemploSpring/ControladorInicio.java
@@ -7,6 +7,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 import lombok.extern.slf4j.Slf4j;
 import mintic.misiontic.ciclo3.modelo.Usuario;
+import java.util.Arrays;
+import java.util.List;
 
 @Controller
 @Slf4j
@@ -27,6 +29,21 @@ public class ControladorInicio {
         u.setNombre("JOHN CARLOS ARRIETA ARRIETA");
         u.setEmail("jarrieta@hotmail.com");
         modelo.addAttribute("alguien", u);
+
+        Usuario u2 = new Usuario();
+        u2.setCedula("777");
+        u2.setClave("El mejor");
+        u2.setNombre("JESUS DE NAZARET");
+        u2.setEmail("jesuscristo@iglesia.com");
+
+        Usuario u3 = new Usuario();
+        u3.setCedula("4321");
+        u3.setClave("*xyz*");
+        u3.setNombre("FULANITO DE TAL");
+        u3.setEmail("fulanito@gmail.com");
+
+        List<Usuario> listaUsuarios = Arrays.asList(u2, u3);
+        modelo.addAttribute("usuarios", listaUsuarios);
 
         log.info("Ejecutando el controlador Inicio MVC");
         return "index";
